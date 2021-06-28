eda31801a118e91342ab50fdac5da0b1e42443a2
pleximaDev <41956118+pleximaDev@users.noreply.github.com>
2021-06-28 12:18:26 +0300

    Added slice of sort functions

--- Algorithms/sorts/main.go
+++ Algorithms/sorts/main.go
@@ -7,45 +7,13 @@ import  (
 )
 
 func main() {
-
-    /*Testcases*/
-    testArrays := [][]int{
-        {9,5,3,5,1,7},
-        {8,5,2,25,6,3,7,3,4,6},
-        {7,4,3,-14,0,8,-1,0,-3,-2,1},
-        {1},
-        {2,1},
-        {},
-        {8,5,2,25,6,3,7,3,4,6,4,7,2,2,3,24,4,8,5,9},
-        {1,2,3,4,5}, /* Note the coma */
-        } // Composite literals
-
-    failedTestcases := []int
-
-    fmt.Println("Amount of testcases: ", len(testArrays), "\n")
-
-    for i, expectation, succes := 0, []int{}, ""; i < len(testArrays); i++ {
-        succes = ""
-        expectation = testArrays[i]
-        sort.Sort(sort.IntSlice(testArrays[i]))
-        fmt.Println("Testcase Nº", i, "\nArray Nº", i+1, "before sorting:\n", testArrays[i])
-        mySorts.BubbleSort(testArrays[i])
-        fmt.Println("Array Nº", i+1, "after sorting:\n", testArrays[i])
-        fmt.Println("Expected:", expectation)
-        if !areSlicesEqual(expectation, testArrays[i]) {
-            succes = "not"
-            failedTestcases = append(failedTestcases, i)
-        }
-        fmt.Println("Testcase has", succes, "been passed")
-        fmt.Printf("\n\n")
+    // mySorts.BubbleSort
+    srts := []func([]int) {
+        mySorts.BubbleSort,
     }
 
-    // if len(failedTestcases) == 0 {
-    if failedTestcases == nil {
-        fmt.Printf("%sPassed all testcases%s\n", "\033[32m", "\033[0m")
-    } else {
-        fmt.Printf("%sFailed testcases\n%v%s\n", "\033[31m", failedTestcases, "\033[0m")
-
+    for _, fnc := range srts {
+        testing(fnc)
     }
 
 }
@@ -58,4 +26,47 @@ func areSlicesEqual(arr1, arr2 []int) bool {
         }
     }
     return true
+}
+
+func testing(srtFnc func([]int)) {
+    /*Testcases*/
+    testArrays := [][]int{
+        {9,5,3,5,1,7},
+        {8,5,2,25,6,3,7,3,4,6},
+        {7,4,3,-14,0,8,-1,0,-3,-2,1},
+        {1},
+        {2,1},
+        {},
+        {8,5,2,25,6,3,7,3,4,6,4,7,2,2,3,24,4,8,5,9},
+        {1,2,3,4,5}, /* Note the coma */
+        } // Composite literals
+
+    var failedTestcases []int
+
+    fmt.Println("Amount of testcases: ", len(testArrays), "\n")
+
+    for i, expectation, succes := 0, []int{}, ""; i < len(testArrays); i++ {
+        succes = ""
+        expectation = testArrays[i]
+        sort.Sort(sort.IntSlice(testArrays[i]))
+        fmt.Println("Testcase Nº", i, "\nArray Nº", i+1, "before sorting:\n", testArrays[i])
+        srtFnc(testArrays[i])
+        fmt.Println("Array Nº", i+1, "after sorting:\n", testArrays[i])
+        fmt.Println("Expected:", expectation)
+        if !areSlicesEqual(expectation, testArrays[i]) {
+            succes = "not"
+            failedTestcases = append(failedTestcases, i)
+        }
+        fmt.Println("Testcase has", succes, "been passed")
+        fmt.Printf("\n\n")
+    }
+    
+    // if len(failedTestcases) == 0 {
+    if failedTestcases == nil {
+        fmt.Printf("%sPassed all testcases%s\n", "\033[32m", "\033[0m")
+    } else {
+        fmt.Printf("%sFailed testcases\n%v%s\n", "\033[31m", failedTestcases, "\033[0m")
+
+    }
+
 }
